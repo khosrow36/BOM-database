c26c5652df1417a8b301bb4622d9e21a0316d1a6
Arti <artek.przystas@gmail.com>
2020-05-23 00:02:58 +0200

    admin panel registration

--- bom/bomapp/admin.py
+++ bom/bomapp/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Supplier, Part, Product
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    fields = ['name', 'parts', 'sell_price']
+
+@admin.register(Part)
+class PartAdmin(admin.ModelAdmin):
+    fields = ['name', 'price', 'supplier']
+
+@admin.register(Supplier)
+class SupplierAdmin(admin.ModelAdmin):
+    fields = ['name', 'info']
+
+
+admin.site.site_header = 'BOM Database Admin Panel'
