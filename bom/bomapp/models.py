822cca633a39c2f75b121041bedfa00c5e44f2bc
Arti <artek.przystas@gmail.com>
2020-05-22 23:26:03 +0200

    models

--- bom/bomapp/models.py
+++ bom/bomapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Suppliers(models.Model):
+    name = models.CharField(max_length=250)
+    info = models.TextField()
+
+class Parts(models.Model):
+    name = models.CharField(max_length=250)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    supplier = models.ForeignKey(Suppliers, on_delete=models.CASCADE)
+
+
+class Products(models.Model):
+    name = models.CharField(max_length=250)
+    parts = models.ForeignKey(Parts, on_delete=models.CASCADE)
+    sell_price = models.DecimalField(max_digits=10, decimal_places=2)
